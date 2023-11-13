9a9d0c98e99bca464b27010edff33a617371372f
dratum <dratumzh187@gmail.com>
2023-11-13 21:53:47 +0300

    Release 4, 5

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 /* eslint-disable linebreak-style */
 const button = document.querySelector('.button-add-word');
 const input = document.querySelector('input');
+const words = document.querySelector('.table-of-words');
 
-function addWord() {
+function addWord(e) {
+  e.preventDefault();
+
   const word = input.value;
-  console.log(word);
+  const newDiv = document.createElement('div');
+  const newSpan = document.createElement('span');
+  newDiv.className = 'russian-word';
+  newSpan.appendChild(document.createTextNode(word));
+  words.appendChild(newDiv);
+  newDiv.appendChild(newSpan);
+  newSpan.appendChild(word);
 }
+
 button.addEventListener('click', addWord);
 input.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
